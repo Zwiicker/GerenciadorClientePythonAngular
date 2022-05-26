a5a46d19b2d1b84c34ebfa976fa1e86dd727f590
Henrique <hickzwicker@gmail.com>
2022-05-26 01:57:09 -0300

    teste

--- UsuarioAPI/UsuarioAPI/wsgi.py
+++ UsuarioAPI/UsuarioAPI/wsgi.py
@@ -3,6 +3,7 @@ WSGI config for UsuarioAPI project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
+uu
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
