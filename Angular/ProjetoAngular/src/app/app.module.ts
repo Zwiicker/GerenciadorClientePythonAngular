be0e1c4255ba574d26618575c2a8a9438eea2cbc
Henrique <hickzwicker@gmail.com>
2022-05-26 12:42:07 -0300

    Adicionado projeto angular , criado componentes e serviços e seus metódos

---
+++ Angular/ProjetoAngular/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { ShowUsuComponent } from './usuario/show-usu/show-usu.component';
+import { AddEditUsuComponent } from './usuario/add-edit-usu/add-edit-usu.component';
+import { UsuarioService } from './usuario.service';
+
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsuarioComponent,
+    ShowUsuComponent,   
+    AddEditUsuComponent     
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [UsuarioService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
