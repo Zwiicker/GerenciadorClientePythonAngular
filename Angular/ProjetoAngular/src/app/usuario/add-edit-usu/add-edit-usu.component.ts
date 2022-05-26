73b0dd8ae0f299424253b3dfd7f6cb611015436b
Henrique <hickzwicker@gmail.com>
2022-05-26 16:37:51 -0300

    Componentes e html feitos.

--- Angular/ProjetoAngular/src/app/usuario/add-edit-usu/add-edit-usu.component.ts
+++ Angular/ProjetoAngular/src/app/usuario/add-edit-usu/add-edit-usu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { UsuarioService } from 'src/app/usuario.service';
 
 @Component({
   selector: 'app-add-edit-usu',
@@ -7,9 +8,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditUsuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:UsuarioService) { }
+
+  @Input() usu: any;
+
+  UsuarioId!:string;
+  UsuarioNome!:string;
+  UsuarioEmail!:string;
+  UsuarioTelefone!:string;
+  UsuarioEndereco!:string;
+  UsuarioProfissao!:string;
+  UsuarioArquivoCurriculo!:string;
+  PhotoFilePath!:string;
+
+  UsuarioList:any=[]
+ 
 
   ngOnInit(): void {
+    this.loadUsuarioList();
   }
 
+  loadUsuarioList(){
+    this.service.getAllUsuarioNames().subscribe((data:any)=>{
+    this.UsuarioId=this.usu.UsuarioId;
+    this.UsuarioNome=this.usu.UsuarioNome;
+    this.UsuarioEmail=this.usu.UsuarioEmail;
+    this.UsuarioTelefone=this.usu.UsuarioTelefone;
+    this.UsuarioEndereco=this.usu.UsuarioEndereco;
+    this.UsuarioProfissao=this.usu.UsuarioProfissao;
+    this.UsuarioArquivoCurriculo=this.usu.UsuarioArquivoCurriculo;
+    this.PhotoFilePath=this.service.PhotoUrl+this.UsuarioArquivoCurriculo;
+
+  });
+}
+
+addUsuario(){
+  var val = {UsuarioId:this.UsuarioId,
+    UsuarioNome:this.UsuarioNome,
+    UsuarioEmail:this.UsuarioEmail,
+    UsuarioTelefone:this.UsuarioTelefone,
+    UsuarioEndereco:this.UsuarioEndereco,
+    UsuarioProfissao:this.UsuarioProfissao,
+    UsuarioArquivoCurriculo:this.UsuarioArquivoCurriculo};
+
+  this.service.addUsuario(val).subscribe(res=>{
+    alert(res.toString());
+  });
+}
+
+updateUsuario(){
+  var val = {UsuarioId:this.UsuarioId,
+    UsuarioNome:this.UsuarioNome,
+    UsuarioEmail:this.UsuarioEmail,
+    UsuarioTelefone:this.UsuarioTelefone,
+    UsuarioEndereco:this.UsuarioEndereco,
+    UsuarioProfissao:this.UsuarioProfissao,
+    UsuarioArquivoCurriculo:this.UsuarioArquivoCurriculo};
+
+  this.service.updateUsuario(val).subscribe(res=>{
+  alert(res.toString());
+  });
+}
+
+uploadPhoto(event:any){
+  var file=event.target.files[0];
+  const formData:FormData=new FormData();
+  formData.append('uploadedFile',file,file.name);
+
+  this.service.UploadPhoto(formData).subscribe((data:any)=>{
+    this.UsuarioArquivoCurriculo=data.toString();
+    this.PhotoFilePath=this.service.PhotoUrl+this.UsuarioArquivoCurriculo;
+  })
+}
 }
--- Angular/ProjetoAngular/src/app/usuario/show-usu/show-usu.component.ts
+++ Angular/ProjetoAngular/src/app/usuario/show-usu/show-usu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/usuario.service';
 
 @Component({
   selector: 'app-show-usu',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowUsuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:UsuarioService) { }
+
+  UsuarioList:any=[];
+
+  ModalTitle!:string;
+  ActivateAddEditUsuComp:boolean=false;
+  usu:any;
 
   ngOnInit(): void {
+    this.refreshUsoList();
+  }
+
+  addClick(){
+    this.usu={
+       UsuarioId:0,
+       UsuarioNome:"",
+       UsuarioEmail:"",
+       UsuarioTelefone:"",
+       UsuarioEndereco:"",
+       UsuarioProfissao:"",
+       UsuarioArquivoCurriculo:"",
+    }
+    this.ModalTitle="Adicionar Cliente";
+    this.ActivateAddEditUsuComp=true;
+
+  }
+
+  closeClick(){
+    this.ActivateAddEditUsuComp=false;
+    this.refreshUsoList();
+  }
+
+  editClick(item:any){
+    console.log(item);
+    this.usu=item;
+    this.ModalTitle="Editar Usuário";
+    this.ActivateAddEditUsuComp=true;
+  }
+
+  deleteClick(item:any){
+    if(confirm('Tem certeza que quer excluir esse cliente??')){
+      this.service.deleteUsuario(item.UsuarioId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshUsoList();
+      })
+    }
+  }
+
+  refreshUsoList(){
+    this.service.getUsoList().subscribe(data=>{
+      this.UsuarioList=data;
+    });
   }
 
 }
