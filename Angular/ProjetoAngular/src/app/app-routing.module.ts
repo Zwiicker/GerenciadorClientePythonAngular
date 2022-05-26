d03ad9f9e17a71d6fa9583379303471292edfdc2
Henrique <hickzwicker@gmail.com>
2022-05-26 13:19:08 -0300

    Coloando Boostrap e mudança no routing;

--- Angular/ProjetoAngular/src/app/app-routing.module.ts
+++ Angular/ProjetoAngular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {UsuarioComponent} from './usuario/usuario.component';
+
+const routes: Routes = [
+  {path:'usuario',component:UsuarioComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
